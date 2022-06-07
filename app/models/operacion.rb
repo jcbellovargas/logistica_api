357850c200f02b0f8633beaee2003aeb9acb46bf
Juan Vargas <itwarp_jvargas@iseatz.com>
2022-06-06 22:06:32 -0300

    Operacion serializer

--- app/models/operacion.rb
+++ app/models/operacion.rb
@@ -3,4 +3,16 @@ class Operacion < ApplicationRecord
   belongs_to :origen, class_name: "Local"
   belongs_to :destino, class_name: "Local"
   belongs_to :producto
+
+  def as_json
+    super(
+      include: [
+        origen: { only: [:id, :nombre, :localidad, :provincia, :tipo_local] },
+        destino: { only: [:id, :nombre, :localidad, :provincia, :tipo_local] }, 
+        producto: { only: [:id, :nombre, :modelo] }, 
+        usuario: { only:[:id, :nombre] }
+      ],
+      only: [:id, :origen, :destino, :producto, :usuario, :cantidad]
+    )
+  end
 end

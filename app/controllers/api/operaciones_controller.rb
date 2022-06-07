2d685ddc9a5e9f7f7d1d7a37b1d1c6d8cfde7906
Juan Vargas <itwarp_jvargas@iseatz.com>
2022-06-06 22:07:03 -0300

    Filter by destino

--- app/controllers/api/operaciones_controller.rb
+++ app/controllers/api/operaciones_controller.rb
@@ -6,7 +6,7 @@ class Api::OperacionesController < ApplicationController
   end
 
   def create
-    params[:usuario_id] = params[:usuario_id] || Usuario.first.id
+    params[:usuario_id] = params[:usuario_id] || Usuario.first.id #TODO: manejo de usuario
     result = Transfer.call(params)
 
     if result.success?
@@ -14,5 +14,15 @@ class Api::OperacionesController < ApplicationController
     else
       render json: { status: "ERROR", mensaje: result.mensaje }
     end
+  end
+
+  def get_by_destino
+    result = Operacion.where(destino_id: params[:destino_id].to_i)
+
+    render json: result.as_json
+  end
+
+  def destinos_mas_operados
+
   end
 end
